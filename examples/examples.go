7cdc967049e9e1a53e3e7b4b7311a290683753d5
Kevin Paulisse <kpaulisse@users.noreply.github.com>
2020-05-24 23:15:58 -0500

    Make all functions exported

--- examples/examples.go
+++ examples/examples.go
@@ -1,16 +1,18 @@
 package examples
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func exampleFuncWithError(s string) error {
+func ExampleFuncWithError(s string) error {
 	return fmt.Errorf("Error: %s", s)
 }
 
-func exampleFuncWithResult(s string) string {
+func ExampleFuncWithResult(s string) string {
 	return fmt.Sprintf("Hello: %s", s)
 }
 
-func exampleFuncWithResultAndError(s string) (int, error) {
+func ExampleFuncWithResultAndError(s string) (int, error) {
 	return 2, fmt.Errorf("Hello: %s", s)
 }
 
@@ -18,14 +20,22 @@ type stru struct {
 	foo string
 }
 
-func (s *stru) exampleFuncWithNoParams() {
+func (s *stru) ExampleFuncWithNoParams() {
 	s.foo = "Hello"
 }
 
-func (s *stru) exampleFuncWithParam(x string) {
+func (s *stru) ExampleFuncWithParam(x string) {
 	s.foo = x
 }
 
-func (s stru) exampleFuncWithReturn() string {
+func (s stru) ExampleFuncWithReturn() string {
 	return s.foo
+}
+
+func (s stru) ExampleFuncWithParamAndReturnAndError(x string) (string, error) {
+	if x == "" {
+		return s.foo, nil
+	} else {
+		return "", fmt.Errorf("Error: %s", x)
+	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/kpaulisse/gotests-template
 
 go 1.13
 
-require github.com/stretchr/testify v1.5.1
+require (
+	github.com/stretchr/testify v1.5.1
+	golang.org/x/tools v0.0.0-20200522201501-cb1345f3a375 // indirect
+)
